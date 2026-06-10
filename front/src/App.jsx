b0c1445dd0bdf108febe4e3754ae65dc5cacc9e0
Mena <dayjacs03@gmail.com>
2026-06-10 15:01:02 -0500

    setting up outlet, nav, possible imgs to use

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import { useState, useEffect } from "react";
+import { Outlet } from "react-dom";
+import { Container } from "@mui/material";
+import Nav from "./nav";
+
+function App() {
+  return (
+    <Container>
+      <Nav></Nav>
+      <Outlet context={{}}></Outlet>
+    </Container>
+  );
+}
 
 export default App;
